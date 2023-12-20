2d291a53af48d1bf8f6875daeb275f126b389df3
Serhii Plis <serzh2577@gmail.com>
2023-12-21 00:23:58 +0300

    adds date in input

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,17 +1,56 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/material_red.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const options = {
+const inputDateRef = document.querySelector('#datetime-picker');
+const btnStartRef = document.querySelector('[data-start]');
+
+inputDateRef.flatpickr({
   enableTime: true,
   time_24hr: true,
+  enableSeconds: true,
+  dateFormat: 'd.m.Y H:i',
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() > Date.now()) {
+      btnStartRef.removeAttribute('disabled');
+    } else {
+      btnStartRef.setAttribute('disabled', 'disabled');
+      console.log(3);
+    }
+
+    // console.log(selectedDates[0].getTime());
+    // console.log(Date.now());
   },
-};
+});
+
+// inputDateRef.addEventListener('blur', onXxx);
+
+btnStartRef.addEventListener('click', onOoo);
+
+// function onXxx(e) {
+//   console.log(2);
+// }
+
+function onOoo(e) {
+  console.log(1);
+
+  btnStartRef.setAttribute('disabled', 'disabled');
+}
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   dateFormat: 'Y-m-d H:i',
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
