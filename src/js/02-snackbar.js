ae132300de35285252dd76c2cde3faa568a4bece
Serhii Plis <serzh2577@gmail.com>
2023-12-25 23:49:48 +0300

    task2

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,5 +1,48 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-`✅ Fulfilled promise in ${delay}ms`;
-`❌ Rejected promise in ${delay}ms`;
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onOoo);
+
+function onOoo(e) {
+  e.preventDefault();
+  let stateBool;
+
+  if (formRef.elements.state.value === 'fulfilled') {
+    stateBool = true;
+  } else {
+    stateBool = false;
+  }
+
+  createPromise(stateBool, formRef.elements.delay.value)
+    .then(({ delay }) => {
+      iziToast.success({
+        position: 'topRight',
+        messageColor: 'brown',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        timeout: 3000,
+      });
+    })
+    .catch(({ delay }) => {
+      iziToast.error({
+        position: 'topRight',
+        messageColor: 'brown',
+        message: `❌ Rejected promise in ${delay}ms`,
+        timeout: 3000,
+      });
+    });
+
+  function createPromise(state, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state) {
+          resolve({ delay });
+        } else {
+          reject({ delay });
+        }
+      }, delay);
+    });
+  }
+
+  e.currentTarget.reset();
+}
